f9386ecc92e2d0cf32bcf56af0714a50f67dc686
Per Magnusson <killingfloor00@gmail.com>
2014-08-26 17:57:09 +0200

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
     res.render('index', {
-        title: 'Node.js/Express test app with PostgreSQL',
+        title: 'Node.js/Express/PostgreSQL test app',
         description: 'Deployed at Heroku for learning purpose.'
     });
 });
